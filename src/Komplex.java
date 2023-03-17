8233e5cba6f904bd6d3160a44e8f34c6a5c0796a
Gabbe16 <94972340+Gabbe16@users.noreply.github.com>
2023-03-17 11:11:18 +0100

    Gjort klart addition med komplexa tal

--- src/Komplex.java
+++ src/Komplex.java
@@ -1,18 +1,47 @@
+import java.util.Objects;
+
 public class Komplex {
-    double a;
-    double b;
+    int a;
+    int b;
+
 
     public Komplex(double a, double b) {
-        this.a = a;
-        this.b = b;
+        this.a = (int) a;
+        this.b = (int) b;
+
     }
 
     public Komplex add(Komplex b) {
+        int ca = (this.a+b.a);
+        int cb = (this.b+b.b);
+        return new Komplex(ca, cb);
     }
 
     public Komplex multiply(Komplex b) {
+        return b;
     }
 
     public double arg() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Komplex komplex = (Komplex) o;
+        return a == komplex.a && b == komplex.b;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+
+    @Override
+    public String toString() {
+        return "" +
+                "" + a +
+                "+" + b + 'i';
     }
 }
--- src/KomplexaTal.java
+++ src/KomplexaTal.java
@@ -1,8 +1,8 @@
 public class KomplexaTal {
     public static void main(String[] args) {
 
-            Komplex a = new Komplex(3.0,2.0);
-            Komplex b = new Komplex(4.0,1.0);
+            Komplex a = new Komplex(3,2);
+            Komplex b = new Komplex(4,1);
 
             System.out.println("talet borde vara 3 + 2i:\n" + a);
 
@@ -29,4 +29,3 @@ public class KomplexaTal {
         }
 
     }
-}
